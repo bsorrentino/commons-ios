c44396494f53d0aabbd16a4b0f5bc4428ff17645
bsorrentino <bartolomeo.sorrentino@gmail.com>
2014-04-02 23:22:24 +0200

    D&D initial import

---
+++ DragDrop/DDTableViewManager.h
@@ -0,0 +1,72 @@
+//
+//  DDManager.h
+//  DragDrop
+//
+//  Created by Bartolomeo Sorrentino on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define _USE_ARC  0
+
+
+#if _USE_ARC == 1
+
+#   define WEAK __weak
+
+#else 
+
+#   define WEAK 
+
+#endif
+
+
+@protocol DDTableViewManagerDelegate <NSObject>
+
+@required
+
+@optional
+
+- (BOOL) possibleDropTo:(NSIndexPath *)target;
+- (BOOL) beginDrag:(NSIndexPath *)source;
+- (void) dropTo:(NSIndexPath *)source target:(NSIndexPath *)target;
+
+@end
+
+
+@interface DDTableViewManager : NSObject<UIGestureRecognizerDelegate> {
+
+@private
+
+    NSIndexPath *source_;
+    
+    UILongPressGestureRecognizer *longPressRecognizer_;
+    UIPanGestureRecognizer *panRecognizer_;
+
+    UITableView *tableView_;
+    
+    WEAK id<DDTableViewManagerDelegate> __unsafe_unretained delegate;
+    
+    dispatch_queue_t scrolling_queue;
+    
+}
+
+@property (nonatomic,readonly) NSIndexPath *source;
+@property (nonatomic,readonly) UITableView *tableView;
+
+#if _USE_ARC == 1 
+@property (nonatomic,weak) id<DDTableViewManagerDelegate> delegate;
+#else
+@property (nonatomic,unsafe_unretained) id<DDTableViewManagerDelegate> delegate;
+#endif
+@property (nonatomic,assign) BOOL enabled;
+
+- (id)initFromTableView:(UITableView *)view;
+
+ 
+@end
+
+
+
+
